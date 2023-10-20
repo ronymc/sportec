1644323cc6b07139e7dfccf5b2113efc198c6561
Rony Mathew <ronymc@gmail.com>
2023-10-20 12:39:01 +0200

    Rearranged source folders.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './componets/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { MaterialModule } from './shared/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MatchResultsComponent } from './components/match-results/match-results.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, MatchResultsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/components/match-results/match-results.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-match-results',
+  templateUrl: './match-results.component.html',
+  styleUrls: ['./match-results.component.scss']
+})
+export class MatchResultsComponent {
+}
